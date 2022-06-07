2568c1971c1a03b4bb60976b53cf99e602d01be4
dactrong <trongndph13723@fpt.edu.vn>
2022-06-07 22:29:49 +0700

    feat: giao diện

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChildrenOutletContexts, RouterModule, Routes } from '@angular/router';
+import { ClientComponent } from './layouts/client/client.component';
+import { HomeComponent } from './pages/client/home/home.component';
+import { ProductComponent } from './pages/client/product/product.component';
+import { AboutComponent } from './pages/client/about/about.component';
+import { AdminComponent } from './layouts/admin/admin.component';
+import { ProductListComponent } from './pages/admin/product-list/product-list.component';
+import { ProductFormComponent } from './pages/admin/product-form/product-form.component';
+import { ProductEditComponent } from './pages/admin/product-edit/product-edit.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ClientComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'products',
+        component: ProductComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ]
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      {
+        path: 'products',
+        children: [
+          {
+            path: '',
+            component: ProductListComponent
+
+          },
+          {
+            path: 'create',
+            component: ProductFormComponent
+          },
+          {
+            path: 'edit',
+            component: ProductEditComponent
+          }
+        ]
+
+      }
+    ]
+  }
+]
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClientComponent } from './layouts/client/client.component';
+import { AdminComponent } from './layouts/admin/admin.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/client/home/home.component';
+import { AboutComponent } from './pages/client/about/about.component';
+import { ProductComponent } from './pages/client/product/product.component';
+import { ProductListComponent } from './pages/admin/product-list/product-list.component';
+import { ProductFormComponent } from './pages/admin/product-form/product-form.component';
+import { ProductEditComponent } from './pages/admin/product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    AdminComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductComponent,
+    ProductListComponent,
+    ProductFormComponent,
+    ProductEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
